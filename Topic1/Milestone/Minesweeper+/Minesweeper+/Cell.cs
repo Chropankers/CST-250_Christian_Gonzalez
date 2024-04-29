4c9e3f629e6cf4af057280e67ff9e7cb3b9673d5
Chropankers <51174981+Chropankers@users.noreply.github.com>
2024-04-28 23:51:04 -0400

    Uploaded Topic 1 Milestone 1

---
+++ Topic1/Milestone/Minesweeper+/Minesweeper+/Cell.cs
@@ -0,0 +1,22 @@
+﻿namespace Models
+{
+    //Christian Gonzalez -- CST-250 -- 4/28/24 This is original work, some structural inspiration from https://www.codeproject.com/Articles/5293335/Minesweeper-Game-in-Csharp
+    public class Cell
+    {
+        // Properties
+        public int Row { get; set; } = -1;
+        public int Column { get; set; } = -1;
+        public bool Visited { get; set; } = false;
+        public bool Live { get; set; } = false;  // True if the cell contains a bomb
+        public int LiveNeighbors { get; set; } = 0;  // Count of live neighbors
+
+        // Constructor
+        public Cell(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+
+        // Additional methods go here later
+    }
+}
