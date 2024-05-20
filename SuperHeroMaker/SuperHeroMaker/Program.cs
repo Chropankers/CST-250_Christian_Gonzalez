d75b2cf52f8e230fed84f73fdd383019039f52ea
Chropankers <51174981+Chropankers@users.noreply.github.com>
2024-05-19 20:30:03 -0400

    Add files via upload

---
+++ SuperHeroMaker/SuperHeroMaker/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+// Christian Gonzalez/CST-250/This is a work based upon the Activity 4 Assignment for Programming in C# II, Grand Canyon University
+namespace SuperHeroMaker
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
